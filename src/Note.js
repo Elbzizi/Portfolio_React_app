95850a8ba0fd086e7eda0f00e249456099768ed0
omar <omar@tzst.com>
2024-04-04 23:51:58 +0200

    Project react Portfolio

--- src/Note.js
+++ src/Note.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Note = () => {
   return (
     <div>
-      
+      <h1>Note </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam corrupti
+        odio molestias odit? Magni, nam porro error eveniet quibusdam,
+        voluptates consectetur omnis officia tempore deleniti obcaecati eius
+        consequatur, sint debitis!
+      </p>
+      <button>focuse</button>
     </div>
   );
-}
+};
 
 export default Note;
