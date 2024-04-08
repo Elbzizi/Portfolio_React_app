3589336e5d9dfaf4d0f5abb5d8bfe72a76544aac
omar <omar@tzst.com>
2024-04-08 13:21:29 +0200

    Project react Portfolio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import Amtal from "./component/Amtal";
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<App />} />
+      <Route path="/Amtal" element={<Amtal />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
