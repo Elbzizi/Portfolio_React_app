443aae77ff7047e391470338cb08682cb6d4766d
omar <omar@tzst.com>
2024-04-10 22:08:51 +0200

    my project react portfolio

---
+++ src/data/Images.js
@@ -0,0 +1,62 @@
+import rome from "../images/rome.jpg";
+import petra from "../images/petra.jpg";
+import marrakech from "../images/marrakech.jpg";
+import manhattan from "../images/manhattan.jpg";
+import damascus from "../images/damascus.jpg";
+import cappadocia from "../images/cappadocia.jpg";
+import cairo from "../images/cairo.jpg";
+import barcelona from "../images/barcelona.jpg";
+
+const IMAGES = [
+  {
+    id: 1,
+    title: "rome",
+    description: "rome rome rome rome",
+    image: rome,
+  },
+
+  {
+    id: 2,
+    title: "petra",
+    description: "petra petra petra petra",
+    image: petra,
+  },
+  {
+    id: 3,
+    title: "marrakech",
+    description: "marrakech marrakech marrakech marrakech",
+    image: marrakech,
+  },
+  {
+    id: 4,
+    title: "manhattan",
+    description: "manhattan manhattan manhattan manhattan",
+    image: manhattan,
+  },
+  {
+    id: 5,
+    title: "damascus",
+    description: "damascus damascus damascus damascus",
+    image: damascus,
+  },
+  {
+    id: 6,
+    title: "cappadocia",
+    description: "cappadocia cappadocia cappadocia cappadocia",
+    image: cappadocia,
+  },
+  {
+    id: 7,
+    title: "cairo",
+    description: "cairo cairo cairo cairo",
+    image: cairo,
+  },
+  {
+    id: 8,
+    title: "barcelona",
+    description: "barcelona barcelona barcelona barcelona",
+    image: barcelona,
+  },
+];
+
+export default IMAGES;
